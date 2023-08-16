2a0afa94cb36b84e14e785bea049f897a9d30881
thawzintoe <thaw.toe@7peakssoftware.com>
2023-08-16 12:03:24 +0700

    add icon for Compose

--- shared/src/commonMain/kotlin/com/ptut/movie/app/shared/App.kt
+++ shared/src/commonMain/kotlin/com/ptut/movie/app/shared/App.kt
@@ -1,14 +1,25 @@
 package com.ptut.movie.app.shared
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Android
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.resources.ExperimentalResourceApi
 
+@OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
     MaterialTheme {
@@ -20,9 +31,20 @@ fun App() {
                 modifier = Modifier.fillMaxSize(),
                 contentAlignment = Alignment.Center,
             ) {
-                Text(
-                    text = "Hello, ${getPlatformName()}!"
-                )
+                Column(
+                    modifier = Modifier.wrapContentSize()
+                ) {
+                    Text(
+                        text = "Hello, ${getPlatformName()}!",
+                    )
+                    Spacer(Modifier.height(16.dp))
+                    Icon(
+                        imageVector = Icons.Rounded.Android,
+                        contentDescription = null,
+                        modifier = Modifier.size(150.dp),
+                        tint = MaterialTheme.colorScheme.primary,
+                    )
+                }
             }
         }
     }
